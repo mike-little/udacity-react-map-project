c280781cf0c4c1e8e3407202bd49c90407d95af6
Mike Little <mklittle@gmail.com>
2019-04-14 11:34:54 -0600

    Add files via upload

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <div>
+                      <h1>Something went wrong.</h1>
+                      <p>Sorry we encountered a problem, please try again.  If the problem persists contact the support team.</p>
+                    </div>;
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
